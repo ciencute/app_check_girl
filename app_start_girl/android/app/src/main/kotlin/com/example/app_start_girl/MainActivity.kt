3eca38e3b9f223b22eb85f86ec7e5ce2eb46108d
Gotrust-PhucNguyen <phuc.nguyen@gotrust.vn>
2022-08-05 10:22:09 +0700

    structer code bloc

---
+++ app_start_girl/android/app/src/main/kotlin/com/example/app_start_girl/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_start_girl
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- check_girl/android/app/src/main/kotlin/com/example/check_girl/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.check_girl
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
